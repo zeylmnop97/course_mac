3075a94cc8fc80287b2886fede7bc7d5e60dcfd0
Zeyad Ahmed <zahmed.bese16seecs@seecs.edu.pk>
2019-07-03 13:15:15 +0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'react-bootstrap'
+import { Navbar,NavbarBrand } from 'reactstrap'
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-    </Navbar>
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+        </Navbar>
+      <Menu/>
     </div>
   );
 }
